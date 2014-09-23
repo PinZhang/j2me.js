38b27ac671e880da595a5abe9988b5114eb060bb
Yuan Xulei <xyuan@mozilla.com>
2014-09-23 11:24:13 +0800

    Add missing files.

---
+++ java/custom/com/nokia/mid/ui/lcdui/LCDUIUtils.java
@@ -0,0 +1,17 @@
+package com.nokia.mid.ui.lcdui;
+
+import javax.microedition.lcdui.Display;
+
+public class LCDUIUtils {
+  static java.lang.Object 	getObjectTrait(java.lang.Object target, java.lang.String traitName) {
+    System.out.println("getObjectTrait");
+    return null;
+  }
+  static boolean 	setObjectTrait(java.lang.Object target, java.lang.String traitName, java.lang.Object value) {
+    return false;
+  }
+  
+  static boolean isDisplayActive(Display display) {
+    return true;
+  }
+}
